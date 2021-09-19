8e46d844a8638d08e990f46ba07570fbb4ae00d0
FourHyphen <FourHyphen.4@gmail.com>
2021-09-19 11:50:44 +0900

    テストプロジェクトを追加

---
+++ TestMyDeliverable/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMyDeliverable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMyDeliverable")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ca9fd98-eb4a-48e2-b3de-c4b219f61672")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMyDeliverable/TestMyUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMyDeliverable
+{
+    [TestClass]
+    public class TestMyUtility
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
