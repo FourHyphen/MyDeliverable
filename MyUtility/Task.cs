c9e8d58030d81e7c7a89f37eff00debc86a2ca06
FourHyphen <FourHyphen.4@gmail.com>
2021-10-17 12:30:10 +0900

    Task.Delayの使い方をTaskクラスに記載

---
+++ MyUtility/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace MyUtility
+{
+    class Task
+    {
+        private void ExampleDelay()
+        {
+            DelayCore();
+        }
+
+        private async void DelayCore()
+        {
+            // 非同期待機: Sleep()だと同期待機なのでブロックしてしまう
+            await System.Threading.Tasks.Task.Delay(1000);
+        }
+    }
+}
