7b18e7255a74430e5f3075386a2140bf28cc07fb
FourHyphen <FourHyphen.4@gmail.com>
2021-10-17 12:23:20 +0900

    Soundクラスを実装

---
+++ MyUtility/Sound.cs
@@ -0,0 +1,31 @@
+﻿namespace MyUtility
+{
+    public class Sound
+    {
+        private System.Media.SoundPlayer Player { get; }
+
+        public Sound(string wavFilePath)
+        {
+            CheckExistFile(wavFilePath);
+            Player = new System.Media.SoundPlayer(wavFilePath);
+
+            // 初期化に時間かかるので前もって完了させておく
+            Player.Play();
+            Player.Stop();
+        }
+
+        public void CheckExistFile(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+            {
+                throw new System.IO.FileNotFoundException(filePath);
+            }
+        }
+
+        public void Play()
+        {
+            Player.Stop();    // 重複して鳴らさないようにする
+            Player.Play();
+        }
+    }
+}
