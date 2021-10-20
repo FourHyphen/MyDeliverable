266b3abc29596d68675b1ad7202ba14157e5b091
FourHyphen <FourHyphen.4@gmail.com>
2021-10-21 07:55:53 +0900

    TestInitとTestCleanupを実装、動作確認

--- TestWpfDeliverable/TestFeature.cs
+++ TestWpfDeliverable/TestFeature.cs
@@ -1,14 +1,47 @@
-﻿using System;
+﻿using Codeer.Friendly.Windows;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 
 namespace TestWpfDeliverable
 {
+    // 必要なパッケージ
+    //  -> Codeer.Friendly
+    //  -> Codeer.Friendly.Windows         -> WindowsAppFriend()
+    //  -> Codeer.Friendly.Windows.Grasp   -> WindowControl()
+    //  -> RM.Friendly.WPFStandardControls -> 各種WPFコントロールを取得するために必要
+
     [TestClass]
     public class TestFeature
     {
-        [TestMethod]
-        public void TestMethod1()
+        private string AttachExeName = "WpfDeliverable.exe";
+        private WindowsAppFriend App;
+
+        [TestInitialize]
+        public void Init()
         {
+            string exePath = System.IO.Path.GetFullPath(AttachExeName);
+            App = new WindowsAppFriend(Process.Start(exePath));
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CloseWindow();
+            App.Dispose();
+        }
+
+        private void CloseWindow()
+        {
+            Process exeMainProcess = Process.GetProcessById(App.ProcessId);
+            exeMainProcess.CloseMainWindow();
+            exeMainProcess.Dispose();
+        }
+
+        [TestMethod]
+        public void TestInitAndCleanup()
+        {
+            // Init による MainWindow 立ち上げと Cleanup による MainWindow クローズが上手くいけば OK
         }
     }
 }
